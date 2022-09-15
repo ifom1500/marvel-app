224813372a7e993d9542d736f0460ff38cde254d
Roman Fomin <rs.fomin@bpcbt.com>
2022-09-15 08:01:09 +0300

    random character

---
+++ src/services/MarvelService.js
@@ -0,0 +1,39 @@
+export default class MarvelService {
+  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  _apiKey = '48c8e0da4e69f811bcca0bd32dbffe50';
+
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&apikey=${this._apiKey}`);
+    return res.data.results.map(this._tranformCharacter);
+  };
+
+  getCharacter = async (id) => {
+    const res = await this.getResource(`${this._apiBase}characters/${id}?apikey=${this._apiKey}`);
+    return this._tranformCharacter(res.data.results[0]);
+  };
+
+  _tranformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
+  };
+}
+
+// Your public key: 48c8e0da4e69f811bcca0bd32dbffe50
+// Your private key: c67efc6299052877d4ac180f6ff78b5dfbb1657d
+// https://developer.marvel.com/account
+// https://developer.marvel.com/docs
